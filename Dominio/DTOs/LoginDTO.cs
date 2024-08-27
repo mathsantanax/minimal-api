5e2d9698977c41ec7d0252cd0a45e460cc38cf3a
Matheus Santana <matheus.santana7@hotmail.com>
2024-08-27 20:53:23 -0300

    iniciando projeto

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+namespace minimal_api.Dominio.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; } = default!;
+        public string Senha { get; set; } = default!;
+
+    }
+}
